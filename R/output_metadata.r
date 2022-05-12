85657c3a04aae6909bfbfdf869a05781e49cf05b
Lucas Fortini <lfortini@usgs.gov>
2022-05-12 13:56:41 -1000

    Initial commit

---
+++ R/output_metadata.r
@@ -0,0 +1,107 @@
+#' create txt file with metadata for r output written to disk
+#'
+#' This function creates a txt file with metadata for any r output written to disk.
+#' The output should help reproducing the analysis as it lists r script file info
+#' along with info workstation info.
+#' 
+#'
+#' @param output_file_nm directory and file name of output
+#' @param script.dir directory of code repository (place it on top of r script)
+#' @param script.name.info name of script used to generate it (place it on top of r script)
+#' @param notes any text string to be added to the text file
+#' @return text file with same name as output file with a lot of metadata info
+#' @export
+output_meta_data_fx=function(output_file_nm, script.dir=script.dir.info, script.name=script.name.info, get.script.line.number=T, notes=""){
+  if (get.script.line.number){
+    savehistory(file = "temp.Rhistory")
+    jnk2=readLines("temp.Rhistory")
+    last_line=jnk2[length(jnk2)-1]
+    last_line=trimws(last_line, which = c("both"))
+    
+    r_file_lines=readLines(paste0(script.dir.info, script.name.info))
+    r_file_lines=trimws(r_file_lines, which = c("both")) #remove left white space
+    #r_file_lines[289]
+    line_number=which(r_file_lines==last_line)
+    
+  }else{
+    line_number=NA
+  }
+  
+  wd.info=getwd()
+  date.info=Sys.time()
+  user.info=Sys.info() ["user"][[1]]
+  R.version.info=R.version.string
+  computer.name=Sys.info() ["nodename"][[1]]
+  computer.sysname.info=Sys.info() ["sysname"][[1]]
+  computer.release.info=Sys.info() ["release"][[1]]
+  computer.info=paste(computer.sysname.info, computer.release.info)
+  
+  # #find location of script based on here package
+  # location_here=tryCatch({
+  #   options(warn=-1) #suppress warnings
+  #   location_here=here::here()
+  # }, warning = function(war) {
+  #   print(paste("MY_WARNING:  ",war))    
+  # }, error = function(err) {    
+  #   #print(paste("MY_ERROR:  ",err))
+  #   location_here="error"
+  # }, finally = {
+  #   options(warn=0)
+  # }) # END tryCatch
+  
+  ####################################
+  #get git commit version!
+  #script.dir="D:/projects/Invasives_modeling/IS_V2_repo/"
+  if (git2r::in_repository(path = script.dir)){ #is script part of repository?
+    r = git2r::revparse_single(script.dir,"HEAD")
+    gt_txt="Code part of git repository"
+    gt_repo=r$repo #branch and latest comit
+    gt_sha=sha(r) #sha id
+    gt_author=r$author #comit author info
+    gt_summary=r$summary #comit summary
+    gt_message=r$message #comit message
+  }else{
+    gt_txt="Code not in git repository"
+    gt_repo=NA
+    gt_sha=NA
+    gt_author=NA
+    gt_summary=NA
+    gt_message=NA
+    
+  }
+  
+  fileConn<-file(paste0(output_file_nm, ".info.txt"))
+  writeLines(c(
+    paste0("output produced on ", as.character(date.info)),
+    paste0("at working directory ", as.character(wd.info)),
+    paste0("saved to file-path ", output_file_nm),
+    paste0("by ", as.character(user.info)),
+    paste0("using script ", as.character(script.name)),
+    paste0("line number ", as.character(line_number)),
+    paste0("located at ", as.character(script.dir)),
+    paste0("using ", as.character(R.version.info)),
+    paste0("on computer ", as.character(computer.name)),
+    paste0("computer info:", as.character(computer.info)),  
+    #paste0("based on here package, project location is: ", as.character(location_here)),  
+    paste0("Notes: ", as.character(notes)), 
+    paste0("#####################################"), 
+    paste0("#####################################"), 
+    as.character(gt_txt), 
+    paste0("branch and latest comit"), #
+    capture.output(gt_repo), #
+    paste0("sha comit id"),
+    capture.output(gt_sha), #
+    paste0("comit author info"),
+    capture.output(gt_author), #
+    paste0("comit summary"),
+    capture.output(gt_summary),#
+    paste0("comit message"),
+    capture.output(gt_message), #
+    
+    paste0("#####################################"), 
+    paste0("#####################################"), 
+    paste0("Session info: "), #, "\n", sessionInfo() 
+    capture.output(sessionInfo()) #for ones loads #https://stackoverflow.com/questions/21967254/how-to-write-a-reader-friendly-sessioninfo-to-text-file
+  ), fileConn)
+  close(fileConn)  
+}
